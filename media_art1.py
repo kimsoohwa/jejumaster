073a90289db00f1d7fe00cf12c92834aeb84be66
kimsoohwa <147927298+kimsoohwa@users.noreply.github.com>
2023-10-16 11:35:02 +0900

    Add files via upload
    저장소에 처음 업로드

---
+++ media_art1.py
@@ -0,0 +1,12 @@
+import turtle as t
+t.speed(0)
+t.pencolor("green")
+
+for i in range(200):
+    t.fd(i)
+    t.lt(90)
+t.done()
+
+
+#colors=['red','green','blue','yellow']
+#    t.pencolor(colors[i%4])
